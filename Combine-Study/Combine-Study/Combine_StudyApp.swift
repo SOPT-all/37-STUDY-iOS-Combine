ff185812cc602553ee6fb3f119d47ee604d1743a
yeonee <leenayeon@sookmyung.ac.kr>
2025-12-16 00:51:33 +0900

    [init] 프로젝트 세팅

---
+++ Combine-Study/Combine-Study/Combine_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_StudyApp.swift
+//  Combine-Study
+//
+//  Created by 이나연 on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Combine_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Combine-Study/Combine-Study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Combine-Study
+//
+//  Created by 이나연 on 12/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
