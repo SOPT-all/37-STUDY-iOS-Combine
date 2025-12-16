a2253dd783d585ab6a6e84d4bf1a304758086fb9
yeonee <leenayeon@sookmyung.ac.kr>
2025-12-17 01:31:38 +0900

    [feat] model

---
+++ Combine-Study/Combine-Study/DailyBoxOfficeModel.swift
@@ -0,0 +1,16 @@
+//
+//  DailyBoxOfficeModel.swift
+//  Combine-Study
+//
+//  Created by 이나연 on 12/16/25.
+//
+
+import Foundation
+
+struct DailyBoxOfficeModel {
+    let rank: String
+    let movieNm: String
+    let movieCd: String
+    let openDt: String
+}
+
