8d7af77c329a0ef74e5850741c0b81530fd383d8
soo127 <ss_127@naver.com>
2025-12-17 02:08:23 +0900

    feat: model

--- Combine-Study/Combine-Study/BoxOfficeResponse.swift
+++ Combine-Study/Combine-Study/BoxOfficeResponse.swift
@@ -5,23 +5,27 @@
 //  Created by 이상수 on 12/17/25.
 //
 
-struct BoxOfficeResponse: Decodable {
-    let boxOfficeResult: BoxOfficeResult
-}
-
-struct BoxOfficeResult: Decodable {
-    let boxofficeType: String
-    let showRange: String
-    let dailyBoxOfficeList: [DailyBoxOffice]
-}
-
-struct DailyBoxOffice: Decodable {
-    let rank: String
-    let rankInten: String
-    let movieNm: String
-    let openDt: String
-    let salesAcc: String
-    let audiCnt: String
-    let audiChange: String
-    let audiAcc: String
+extension MovieAPI {
+    
+    struct BoxOfficeResponse: Decodable {
+        let boxOfficeResult: BoxOfficeResult
+    }
+    
+    struct BoxOfficeResult: Decodable {
+        let boxofficeType: String
+        let showRange: String
+        let dailyBoxOfficeList: [DailyBoxOffice]
+    }
+    
+    struct DailyBoxOffice: Decodable {
+        let rank: String
+        let rankInten: String
+        let movieNm: String
+        let openDt: String
+        let salesAcc: String
+        let audiCnt: String
+        let audiChange: String
+        let audiAcc: String
+    }
+    
 }
