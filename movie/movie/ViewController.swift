8ea43b734599927d0a1a7b93b2fba8c5876f4539
dudwntjs <sts07190@naver.com>
2025-12-16 22:39:40 +0900

    [setting] 프로젝트 세팅

---
+++ movie/movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie
+//
+//  Created by sun on 12/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
